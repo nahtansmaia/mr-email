658abf793f211a7c1a0eedeeab9d3dc55eb8d230
Nathan Maia <nahtansmaia@gmail.com>
2021-08-28 14:08:14 -0300

    Implementado RabbitMQConfig.

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -3,7 +3,6 @@ package com.ms.email.controllers;
 import com.ms.email.dtos.EmailDTO;
 import com.ms.email.services.EmailService;
 import com.ms.email.models.EmailModel;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +21,6 @@ public class EmailController {
     @PostMapping("/sending-email")
     public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDTO emailDTO) {
         EmailModel email = new EmailModel();
-        BeanUtils.copyProperties(emailDTO, email);
         EmailModel.parser(email, emailDTO);
         emailService.sendEmail(email);
         return new ResponseEntity<>(email, HttpStatus.CREATED);

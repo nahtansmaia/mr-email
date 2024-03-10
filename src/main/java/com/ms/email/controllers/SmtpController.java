3a1504b219575404b1f2f670dfef749ec67117c2
Nathan Maia <nahtansmaia@gmail.com>
2024-03-10 14:08:01 -0300

    refactoring smtp controller;

--- src/main/java/com/ms/email/controllers/SmtpController.java
+++ src/main/java/com/ms/email/controllers/SmtpController.java
@@ -14,34 +14,35 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping(name = "/smtp")
 public class SmtpController {
 
     @Autowired
     SmtpService smtpService;
 
-    @PostMapping("/smtp")
+    @PostMapping
     public ResponseEntity<SmtpResponseDTO> sendingSmtp(@RequestBody @Valid SmtpDTO smtpDTO) {
         SmtpModel smtpModel =  smtpService.saveSmtp(SmtpModel.parser(smtpDTO));
         return new ResponseEntity<>(SmtpResponseDTO.parser(smtpModel), HttpStatus.CREATED);
     }
 
-    @GetMapping("/smtp")
+    @GetMapping
     public ResponseEntity<List<SmtpModel>> listAllSmtp() {
         return new ResponseEntity<>(smtpService.listAllSmtp(), HttpStatus.OK);
     }
 
-    @GetMapping("/smtp/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<SmtpModel> findById(@PathVariable(value = "id") UUID id) {
         return new ResponseEntity<>(smtpService.findById(id), HttpStatus.OK);
     }
 
-    @PutMapping("/smtp")
+    @PutMapping
     public ResponseEntity<SmtpResponseDTO> updateSmtp(@RequestBody @Valid SmtpDTO smtpDTO) {
         SmtpModel smtpModel = smtpService.updateSmtp(SmtpModel.parser(smtpDTO));
         return new ResponseEntity<>(SmtpResponseDTO.parser(smtpModel), HttpStatus.OK);
     }
 
-    @DeleteMapping("/smtp/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteSmtp(@PathVariable("id") UUID id) {
         smtpService.deleteSmtp(id);
